b71a2b0afdaf3eee02551886cc852575c453f920
KenOnLetsNote <feruos@gmail.com>
2019-12-14 06:34:16 +0900

    functions implemented

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 // Variables are set of concetration of key components. Intended to be used as slice or array
 type Variables struct {
 	x float64 //x represents concentration of protonated PMO SM
@@ -27,12 +29,45 @@ var Ini = Initial{
 
 // Constants is set of kinetic and equilibrium constants
 type Constants struct {
-	kc float64 //kc is coupling reaction constant
-	ks float64 //ks is monomer hydrolysis reaction
+	KC float64 //KC is coupling reaction constant
+	KS float64 //KS is monomer hydrolysis reaction
 	K1 float64 //K1 is PMO SM protonation equilibrium constant
 	K2 float64 //K2 is base protonation equilibrium constant
 }
 
+//Cons is a set of constants
+var Cons = Constants{
+	KC: 1.0,
+	KS: 0.1,
+	K1: 4.365e-9,
+	K2: 1.7782e-11,
+}
+
 func main() {
 
+}
+
+func dzdt(Cons Constants, Ini Initial, X Variables) (difZ float64) {
+
+	difZ = Cons.KC * (Ini.PH0 - X.x - X.z) * (Ini.MCl0 - X.z - X.h)
+
+	return
+
+}
+
+func dhdt(Cons Constants, Ini Initial, X Variables) (difH float64) {
+	difH = Cons.KS * (Ini.MCl0 - X.z - X.h)
+	return
+}
+
+func equilibrium(Cons Constants, Ini Initial, X *Variables) {
+	K := Cons.K1 / Cons.K2
+	H := X.z + X.h
+	R := Ini.PH0 - X.z
+	B := Ini.B0 - X.z - X.h
+
+	X.x = (-K*B - H - R + math.Sqrt(math.Pow(K*B+H+R, 2.0)+4.0*(K-1.0)*H*R)) /
+		(2.0 * (K - 1.0))
+
+	X.y = X.z + X.h - X.x
 }
