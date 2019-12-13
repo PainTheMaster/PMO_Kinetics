d95a1e810da972213fa2687e3b26a77715918eb1
KenOnLetsNote <feruos@gmail.com>
2019-12-14 08:34:57 +0900

    RK4 implemented

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 // Variables are set of concetration of key components. Intended to be used as slice or array
 type Variables struct {
@@ -9,9 +12,6 @@ type Variables struct {
 	z float64 //z represents concentration of PMO product
 	h float64 //h represents concentration of monomer hydrolyzed compound
 }
-
-//X is a slice of set of variables (concentrations). To be assigned in main()
-var X []Variables
 
 //Initial are set of initial concentration of key components
 type Initial struct {
@@ -44,15 +44,58 @@ var Cons = Constants{
 }
 
 func main() {
+	var div /*k,*/, iPC /*, back*/ int
+	var tTot, dt float64
 
+	tTot = 20.0
+	div = 10000
+	dt = tTot / float64(div)
+
+	X := make([]Variables, div+1)
+
+	iPC = 0
+	X[0] = Variables{
+		x: 0.0,
+		y: 0.0,
+		z: 0.0,
+		h: 0.0,
+	}
+
+	for iPC = 1; iPC <= 3; iPC++ {
+		var dif1, dif2, dif3, dif4 Variables
+		var temp2, temp3, temp4 Variables
+
+		dif1.z = dzdt(Cons, Ini, X[iPC-1])
+		dif1.h = dhdt(Cons, Ini, X[iPC-1])
+		temp2.z = X[iPC-1].z + dt*dif1.z/2.0
+		temp2.h = X[iPC-1].h + dt*dif1.h/2.0
+		equilibrium(Cons, Ini, &temp2)
+
+		dif2.z = dzdt(Cons, Ini, temp2)
+		dif2.h = dhdt(Cons, Ini, temp2)
+		temp3.z = X[iPC-1].z + dt*dif2.z/2.0
+		temp3.h = X[iPC-1].h + dt*dif2.h/2.0
+		equilibrium(Cons, Ini, &temp3)
+
+		dif3.z = dzdt(Cons, Ini, temp3)
+		dif3.h = dhdt(Cons, Ini, temp3)
+		temp4.z = X[iPC-1].z + dt*dif3.z
+		temp4.h = X[iPC-1].h + dt*dif3.h
+		equilibrium(Cons, Ini, &temp4)
+
+		dif4.z = dzdt(Cons, Ini, temp4)
+		dif4.h = dhdt(Cons, Ini, temp4)
+
+		X[iPC].z = X[iPC-1].z + (dif1.z+dif2.z*2.0+dif3.z*2.0+dif4.z)*dt/6.0
+		X[iPC].h = X[iPC-1].h + (dif1.h+dif2.h*2.0+dif3.h*2.0+dif4.h)*dt/6.0
+		equilibrium(Cons, Ini, &X[iPC])
+	}
 }
 
 func dzdt(Cons Constants, Ini Initial, X Variables) (difZ float64) {
 
 	difZ = Cons.KC * (Ini.PH0 - X.x - X.z) * (Ini.MCl0 - X.z - X.h)
-
 	return
-
 }
 
 func dhdt(Cons Constants, Ini Initial, X Variables) (difH float64) {
@@ -70,4 +113,6 @@ func equilibrium(Cons Constants, Ini Initial, X *Variables) {
 		(2.0 * (K - 1.0))
 
 	X.y = X.z + X.h - X.x
+
+	fmt.Println("ratio y/x:", X.y/X.x)
 }
