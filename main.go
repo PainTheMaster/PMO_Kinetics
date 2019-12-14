44c35dde7bf197f82fbf4755eb7567f9acc86b9f
KenOnLetsNote <feruos@gmail.com>
2019-12-14 16:21:09 +0900

    Predictor-corrector implemented

--- main.go
+++ main.go
@@ -61,54 +61,80 @@ func main() {
 		h: 0.0,
 	}
 
-	adj := 0.01 //positive value!
-	idx5Hour := int(5.0 / dt)
+	for iPC = 1; iPC <= div; iPC++ {
+		var dif1, dif2, dif3, dif4 Variables
+		var temp2, temp3, temp4 Variables
 
-	for {
-		for iPC = 1; iPC <= div; iPC++ {
-			var dif1, dif2, dif3, dif4 Variables
-			var temp2, temp3, temp4 Variables
+		dif1.z = dzdt(Cons, Ini, X[iPC-1])
+		dif1.h = dhdt(Cons, Ini, X[iPC-1])
+		temp2.z = X[iPC-1].z + dt*dif1.z/2.0
+		temp2.h = X[iPC-1].h + dt*dif1.h/2.0
+		equilibrium(Cons, Ini, &temp2)
 
-			dif1.z = dzdt(Cons, Ini, X[iPC-1])
-			dif1.h = dhdt(Cons, Ini, X[iPC-1])
-			temp2.z = X[iPC-1].z + dt*dif1.z/2.0
-			temp2.h = X[iPC-1].h + dt*dif1.h/2.0
-			equilibrium(Cons, Ini, &temp2)
+		dif2.z = dzdt(Cons, Ini, temp2)
+		dif2.h = dhdt(Cons, Ini, temp2)
+		temp3.z = X[iPC-1].z + dt*dif2.z/2.0
+		temp3.h = X[iPC-1].h + dt*dif2.h/2.0
+		equilibrium(Cons, Ini, &temp3)
 
-			dif2.z = dzdt(Cons, Ini, temp2)
-			dif2.h = dhdt(Cons, Ini, temp2)
-			temp3.z = X[iPC-1].z + dt*dif2.z/2.0
-			temp3.h = X[iPC-1].h + dt*dif2.h/2.0
-			equilibrium(Cons, Ini, &temp3)
+		dif3.z = dzdt(Cons, Ini, temp3)
+		dif3.h = dhdt(Cons, Ini, temp3)
+		temp4.z = X[iPC-1].z + dt*dif3.z
+		temp4.h = X[iPC-1].h + dt*dif3.h
+		equilibrium(Cons, Ini, &temp4)
 
-			dif3.z = dzdt(Cons, Ini, temp3)
-			dif3.h = dhdt(Cons, Ini, temp3)
-			temp4.z = X[iPC-1].z + dt*dif3.z
-			temp4.h = X[iPC-1].h + dt*dif3.h
-			equilibrium(Cons, Ini, &temp4)
+		dif4.z = dzdt(Cons, Ini, temp4)
+		dif4.h = dhdt(Cons, Ini, temp4)
 
-			dif4.z = dzdt(Cons, Ini, temp4)
-			dif4.h = dhdt(Cons, Ini, temp4)
-
-			X[iPC].z = X[iPC-1].z + (dif1.z+dif2.z*2.0+dif3.z*2.0+dif4.z)*dt/6.0
-			X[iPC].h = X[iPC-1].h + (dif1.h+dif2.h*2.0+dif3.h*2.0+dif4.h)*dt/6.0
-			equilibrium(Cons, Ini, &X[iPC])
-		}
-
-		residPMO := Ini.PH0 - X[idx5Hour].z
-		fmt.Println("KC:", Cons.KC, ", 1% residue:", Ini.PH0*0.01, ", 5h residue:", residPMO)
-		if residPMO >= Ini.PH0*0.01*0.99 && residPMO <= Ini.PH0*0.01*1.01 {
-			break
-		} else if residPMO < Ini.PH0*0.01*0.9 {
-			Cons.KC *= (1.0 - adj)
-		} else {
-			Cons.KC *= (1.0 + adj)
-		}
+		X[iPC].z = X[iPC-1].z + (dif1.z+dif2.z*2.0+dif3.z*2.0+dif4.z)*dt/6.0
+		X[iPC].h = X[iPC-1].h + (dif1.h+dif2.h*2.0+dif3.h*2.0+dif4.h)*dt/6.0
+		equilibrium(Cons, Ini, &X[iPC])
 	}
 
-	fmt.Println("Done!")
+	fmt.Println("RK Done!")
+
+	Y := make([]Variables, div+1)
+
+	for iPC = 0; iPC <= 3; iPC++ {
+		Y[iPC] = X[iPC]
+	}
+
+	for iPC = 4; iPC <= div; iPC++ {
+		//Prediction
+		predZ0 := dzdt(Cons, Ini, Y[iPC-4])
+		predZ1 := dzdt(Cons, Ini, Y[iPC-3])
+		predZ2 := dzdt(Cons, Ini, Y[iPC-2])
+		predZ3 := dzdt(Cons, Ini, Y[iPC-1])
+
+		predH0 := dhdt(Cons, Ini, Y[iPC-4])
+		predH1 := dhdt(Cons, Ini, Y[iPC-3])
+		predH2 := dhdt(Cons, Ini, Y[iPC-2])
+		predH3 := dhdt(Cons, Ini, Y[iPC-1])
+
+		Y[iPC].z = Y[iPC-1].z + (55.0*predZ3-59.0*predZ2+37.0*predZ1-9.0*predZ0)*dt/24.0
+		Y[iPC].h = Y[iPC-1].h + (55.0*predH3-59.0*predH2+37.0*predH1-9.0*predH0)*dt/24.0
+
+		equilibrium(Cons, Ini, &Y[iPC])
+
+		//Correction
+		corrZ1 := dzdt(Cons, Ini, Y[iPC-3])
+		corrZ2 := dzdt(Cons, Ini, Y[iPC-2])
+		corrZ3 := dzdt(Cons, Ini, Y[iPC-1])
+		corrZ4 := dzdt(Cons, Ini, Y[iPC-0])
+
+		corrH1 := dhdt(Cons, Ini, Y[iPC-3])
+		corrH2 := dhdt(Cons, Ini, Y[iPC-2])
+		corrH3 := dhdt(Cons, Ini, Y[iPC-1])
+		corrH4 := dhdt(Cons, Ini, Y[iPC-0])
+
+		Y[iPC].z = Y[iPC-1].z + (9.0*corrZ4+19.0*corrZ3-5.0*corrZ2+corrZ1)*dt/24.0
+		Y[iPC].h = Y[iPC-1].h + (9.0*corrH4+19.0*corrH3-5.0*corrH2+corrH1)*dt/24.0
+
+		equilibrium(Cons, Ini, &Y[iPC])
+	}
+
 	for i := range X {
-		fmt.Printf("%f,%f\n", float64(i)*dt, Ini.PH0-X[i].z)
+		fmt.Printf("%f,%.10f,%.10f\n", float64(i)*dt, Ini.PH0-X[i].z, Ini.PH0-Y[i].z)
 	}
 }
 
